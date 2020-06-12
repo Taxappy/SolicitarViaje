ee05d77a708c53a359ff15d1360d9b8c38f05691
eduardo velasquez <eduardo.velasquezv@udea.edu.co>
2020-06-12 02:20:00 -0500

    Websocket run

--- src/main/java/com/taxappy/viaje/config/WebSocketConfig.java
+++ src/main/java/com/taxappy/viaje/config/WebSocketConfig.java
@@ -2,13 +2,13 @@ package com.taxappy.viaje.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket/socket")
--- src/main/java/com/taxappy/viaje/controller/SolicitarViajeController.java
+++ src/main/java/com/taxappy/viaje/controller/SolicitarViajeController.java
@@ -1,25 +1,24 @@
 package com.taxappy.viaje.controller;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
+import java.util.List;
 
 import com.taxappy.viaje.model.TaxistaNotification;
 import com.taxappy.viaje.model.Viaje;
 import com.taxappy.viaje.repository.ViajeRepository;
 import com.taxappy.viaje.service.TaxistaNotificationService;
 import com.taxappy.viaje.stream.TaxistaNotificationStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
@@ -30,6 +29,7 @@ public class SolicitarViajeController {
 	private ViajeRepository viajeRepositoy;
 	@Autowired
 	private TaxistaNotificationService notificacionSaveService;
+	List<TaxistaNotification> ejemploLista = new ArrayList<TaxistaNotification>();
 	private ArrayList <TaxistaNotification> TaxistaNotifications;
 	private final SimpMessagingTemplate template;
 
@@ -45,14 +45,15 @@ public class SolicitarViajeController {
 		notificacionSaveService.saveNotification(notificacion);
 		return viajeRepositoy.save(viaje);
 	}
-	@GetMapping("/{idUsuario}")
-	public Iterable<Viaje> findByIdUsuario(@PathVariable int idUsuario) {
-		return viajeRepositoy.findViajesByIdUsuario(idUsuario);
-	}
+	// @GetMapping("/{idUsuario}")
+	// public Iterable<Viaje> findByIdUsuario(@PathVariable int idUsuario) {
+	// 	return viajeRepositoy.findViajesByIdUsuario(idUsuario);
+	// }
 	@StreamListener(TaxistaNotificationStream.INPUT)
 	public void saveNotification(@Payload TaxistaNotification notificacion) {
-		TaxistaNotifications.add(notificacion);
-		this.template.convertAndSend("/websocket/message",  TaxistaNotifications);
+
+		ejemploLista.add(notificacion);
+		this.template.convertAndSend("/websocket/message",  ejemploLista);
 	}
 
 }
